489fc034845b9b633672f28fdcdccd481f1eb3bf
eduardo borges <84474594+duduborges@users.noreply.github.com>
2023-09-21 15:40:35 +0000

    meudeus q depressao

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import "./assets/App.css"
 import { useState, useEffect } from "react";
 
 
@@ -9,11 +9,21 @@ export default function App() {
     enable: false,
     task: ""
   })
+  const [inputL, setInputL] = useState("")
+  const [links, setLinks] = useState<string[]>([]);
+  const [editLink, SetEditLink] = useState({
+    enable: false,
+    link: ""
+  })
 
   useEffect(() => {
     const tarefasSalvas = localStorage.getItem("Anotações")
     if (tarefasSalvas) {
       setTasks(JSON.parse(tarefasSalvas));
+    }
+    const linksSalvos = localStorage.getItem("Url")
+    if (linksSalvos) {
+      setLinks(JSON.parse(linksSalvos));
     }
   }, [])
 
@@ -21,10 +31,18 @@ export default function App() {
 
   function handleRegister() {
     if (!input) {
-      alert("Invalido! Não tem nada escrito no campo.")
+      alert("Invalido! Não tem nada escrito no campo das tarefas.")
       return
     }
     if (editTask.enable) {
+      handleSaveEdit();
+      return
+    }
+    if (!inputL) {
+      alert("Invalido! Não tem nada escrito no campo dos links.")
+      return
+    }
+    if (editLink.enable) {
       handleSaveEdit();
       return
     }
@@ -32,6 +50,9 @@ export default function App() {
     setTasks(tarefas => [...tarefas, input]);
     setInput("")
     localStorage.setItem("Anotações", JSON.stringify([...tasks, input]))
+    setLinks(caminho => [...caminho, inputL]);
+    setInputL("")
+    localStorage.setItem("Url", JSON.stringify([...links, inputL]))
 
   }
   function handleSaveEdit() {
@@ -47,18 +68,38 @@ export default function App() {
     setInput("")
     localStorage.setItem("Anotações", JSON.stringify(allTasks))
 
+    const findIndexLink = links.findIndex(link => link === editLink.link)
+    const allLinks = [...links];
+
+    allLinks[findIndexLink] = inputL
+    setLinks(allLinks)
+    SetEditLink({
+      enable: false,
+      link: ""
+    })
+    setInputL("")
+    localStorage.setItem("Url", JSON.stringify(allLinks))
+
   }
-  function handleDelete(item: string) {
+  function handleDelete(item: string, linkes: string) {
     const removeTask = tasks.filter(task => task !== item)
     setTasks(removeTask)
     localStorage.setItem("Anotações", JSON.stringify(removeTask))
+    const removeLink = links.filter(link => link !== linkes)
+    setLinks(removeLink)
+    localStorage.setItem("Url", JSON.stringify(removeLink))
 
   }
-  function handleEdit(item: string) {
+  function handleEdit(item: string, linkes: string) {
     setInput(item)
     SetEditTask({
       enable: true,
       task: item
+    })
+    setInputL(linkes)
+    SetEditLink({
+      enable: true,
+      link: linkes
     })
   }
 
@@ -68,22 +109,28 @@ export default function App() {
       <input
         type="text" id="" placeholder="Digite uma tarefa aqui: " value={input} onChange={
           (e) => setInput(e.target.value)
+        } />
+      <input
+        type="text" id="" placeholder="Digite um link aqui: " value={inputL} onChange={
+          (a) => setInputL(a.target.value)
         } /> <br /><br />
-      <button onClick={handleRegister}> {editTask.enable ? "Atualizar tarefa" : "Adicionar tarefa"}</button><br /><br />
+      <button onClick={handleRegister}> {editTask.enable ? "Atualizar tarefa" : "Adicionar tarefa"} {editLink.enable ? "Atualizar tarefa" : "Adicionar tarefa"}</button><br /><br />
 
 
       {tasks.map((item, index) => (
-        <section>
-          <button onClick={() => handleEdit(item)}>Editar</button>
-          <span>
-            {item}
-          </span>
+        <table>
+          <h3>{item}</h3>
+          
+          <button onClick={() => handleEdit(item, linkes)}>Editar</button>
 
-          <button onClick={() => handleDelete(item)}>
-            Excluir
-          </button>
-        </section>
-      ))}
-    </div>
+
+          <button onClick={() => handleDelete(item, links)}>Excluir</button>
+
+
+
+        </table>
+      ))
+      }
+    </div >
   );
 }
